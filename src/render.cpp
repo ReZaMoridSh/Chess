0c589bfb36fac0917b3c80bd3e6f51b951f627be
ReZaMoridSh <reza.m.2001.s.rm@gmail.com>
2022-06-05 19:42:31 +0430

    second commit

--- src/render.cpp
+++ src/render.cpp
@@ -59,13 +59,82 @@ void render(sf::RenderWindow &window)
     Cells cells;
     setting set;
 
-    int r, c;
+    Texture analyze;
+    analyze.loadFromFile(path + "images/" + "Analyze.png");
+    analyze.setSmooth(true);
+    Sprite anz;
+    anz.setTexture(analyze);
+    anz.setPosition(Vector2f(1040, 220));
+    anz.setScale(Vector2f(0.158, 0.158));
+    RectangleShape analyze_rect;
+    analyze_rect.setSize(Vector2f(152, 152));
+    analyze_rect.setPosition(1040, 220);
+    Text def;
+    Font font;
+    font.loadFromFile(path + "fonts/" +"Impact.ttf");
+    def.setFont(font);
+    def.setScale(Vector2f(.8, .8));
+
+
+
+    Texture Restart;
+    Restart.loadFromFile(path + "images/" + "restart.png");
+    Restart.setSmooth(true);
+    Sprite rs;
+    rs.setTexture(Restart);
+    rs.setPosition(Vector2f(830, 220));
+    rs.setScale(Vector2f(0.3, 0.3));
+    Text R;
+    R.setString("Restart Game");
+    R.setPosition(840, 380);
+    R.setFont(font);
+    R.setScale(Vector2f(.8, .8));
+
+
+
+
+
+
+
+
+    Text text;
+    Font f;
+    f.loadFromFile(path + "fonts/" +"Terserah.otf");
+    text.setFont(f);
+    text.setFillColor(Color::White);
+    text.setOutlineThickness(5);
+    text.setOutlineColor(Color(131,122,122));
+    text.setScale(2,2);
+    text.setPosition(840, 70);
+
+
+
+    
+
+
+    static bool clicked;
+    static bool IS_CHECK;
+    static bool IS_CHECKMATE;
+    static string amove;
+    static bool warn;
+    static string defense_warn;
+    static string mate_warn;
+    int r, c,px,py;
+    char type;
 
     while (window.isOpen())
     {
         window.clear(Color::Black);
         window.draw(sp);
+        window.draw(analyze_rect);
+        window.draw(def);
+        window.draw(anz);
+        window.draw(text);
+        window.draw(rs);
+        window.draw(R);
 
+
+        (set.Turn==2 and !IS_CHECKMATE )?text.setString("White's Turn"):text.setString("Black's Turn");
 
         cells.resize(8);
         for (int row = 0; row < 8; row++)
@@ -80,7 +149,115 @@ void render(sf::RenderWindow &window)
             }
         }
 
-        
+        if (clicked)
+        {
+            anz.setColor(Color::White);
+            analyze_rect.setFillColor(Color(37, 183, 211));
+            def.setFillColor(Color::White);
+            def.setString("Start Analyze");
+            def.setPosition(1050, 380);
+            for (int m = 0; m < amove.length(); m = m + 4)
+            {
+                int X_dest = amove[m] - '0';
+                int Y_dest = amove[m + 2] - '0';
+                if (!CurrentBoard.getoccupied(X_dest, Y_dest))
+                {
+                    CircleShape circle;
+                    circle.setRadius(12);
+                    circle.setFillColor(Color(12, 152, 5));
+                    circle.setPosition(set.set_cicle_position(X_dest, Y_dest));
+                    window.draw(circle);
+                }
+                else
+                {
+                    RectangleShape rec;
+                    rec.setSize(Vector2f(70, 10));
+                    rec.setFillColor(Color(2, 152, 5));
+                    rec.setPosition(set.set_rec_position(X_dest, Y_dest));
+                    window.draw(rec);
+                }
+            }
+        }
+        else
+        {
+            anz.setColor(Color(95, 95, 95));
+            analyze_rect.setFillColor(Color(14, 68, 79));
+            def.setFillColor(Color(80, 80, 80));
+            def.setString("Select a piece");
+            def.setPosition(1045, 380);
+        }
+        if (IS_CHECKMATE)
+        {
+            (set.Turn==1 )?text.setString("White wins !!"):text.setString("Black wins !!");
+            int king_x = CurrentBoard.get_king_pos(0, set.Turn);
+            int king_y = CurrentBoard.get_king_pos(1, set.Turn);
+            cells[king_x][king_y].rect.setFillColor(Color(199, 0, 57));
+            window.draw(cells[king_x][king_y].rect);
+        }
+
+        if (IS_CHECK and !IS_CHECKMATE)
+        {
+            int king_x = CurrentBoard.get_king_pos(0, set.Turn);
+            int king_y = CurrentBoard.get_king_pos(1, set.Turn);
+            CircleShape c;
+            c.setRadius(45);
+            c.setFillColor(Color(250, 110, 90));
+            c.setPosition(set.set_cicle2_position(king_x, king_y));
+            window.draw(c);
+        }
+        if (warn)
+        {
+            def.setString("Done !");
+            anz.setColor(Color(0, 255, 0));
+            analyze_rect.setFillColor(Color(0, 183, 0));
+            def.setFillColor(Color::White);
+            def.setPosition(1080, 380);
+            for (int w = 0; w < defense_warn.length(); w = w + 4)
+            {
+                int X_dest = defense_warn[w] - '0';
+                int Y_dest = defense_warn[w + 2] - '0';
+                if (!CurrentBoard.getoccupied(X_dest, Y_dest))
+                {
+                    CircleShape circle;
+                    circle.setRadius(12);
+                    circle.setFillColor(Color::Red);
+                    circle.setPosition(set.set_cicle_position(X_dest, Y_dest));
+                    window.draw(circle);
+                }
+                else
+                {
+                    RectangleShape rec;
+                    rec.setSize(Vector2f(70, 10));
+                    rec.setFillColor(Color::Red);
+                    rec.setPosition(set.set_rec_position(X_dest, Y_dest));
+                    window.draw(rec);
+                }
+            }
+
+            for (int w = 0; w < mate_warn.length(); w = w + 4)
+            {
+                int X_dest = mate_warn[w] - '0';
+                int Y_dest = mate_warn[w + 2] - '0';
+                if (!CurrentBoard.getoccupied(X_dest, Y_dest))
+                {
+                    CircleShape circle;
+                    circle.setRadius(12);
+                    circle.setFillColor(Color::Blue);
+                    circle.setPosition(set.set_cicle_position(X_dest, Y_dest));
+                    window.draw(circle);
+                }
+                else
+                {
+                    RectangleShape rec;
+                    rec.setSize(Vector2f(70, 10));
+                    rec.setFillColor(Color::Blue);
+                    rec.setPosition(set.set_rec_position(X_dest, Y_dest));
+                    window.draw(rec);
+                }
+            }
+        }
+
+
         for (int row = 0; row < 8; row++)
             for (int col = 0; col < 8; col++)
             {
@@ -112,6 +289,91 @@ void render(sf::RenderWindow &window)
                 
             }
 
+
+
+            
+
+            if (event.mouseButton.button == Mouse::Left and clicked)
+            {
+                int mouse_x = event.mouseButton.x;
+                int mouse_y = event.mouseButton.y;
+                if (mouse_x > 1040 and mouse_x < 1190 and mouse_y > 220 and mouse_y < 370)
+                {
+                    if (CurrentBoard.getmove(r, c).length() > 0)
+                    {
+                        defense_warn = WarningMoveDefense(CurrentBoard, r, c, set.Turn);
+                        mate_warn = WarningMoveMate(CurrentBoard, r, c, set.Turn);
+                        warn = true;
+                    }
+                    else
+                        clicked = false;
+                }
+                else
+                    warn = false;
+                // else clicked=false;
+            }
+
+            if (event.mouseButton.button == Mouse::Left && clicked)
+            {
+                int mouse_x = event.mouseButton.x;
+                int mouse_y = event.mouseButton.y;
+                if (mouse_x < set.cell_offset + 8 * set.cell_size && mouse_x > set.cell_offset && mouse_y < set.cell_offset + 8 * set.cell_size && mouse_y > set.cell_offset)
+                {
+                    int r2 = (mouse_x - set.cell_offset) / (set.cell_size);
+                    int c2 = 7 - (mouse_y - set.cell_offset) / (set.cell_size);
+                    for (int a = 0; a < amove.length(); a = a + 4)
+                        if (r2 == amove[a] - '0' and c2 == amove[a + 2] - '0' and set.your_turn(r, c))
+                        {
+
+                            moveit(CurrentBoard, CurrentBoard.gettype(r, c), r, c, r2, c2, CurrentBoard.getcolor(r, c), 5);
+
+                            set.flipturn();
+
+                            if (Is_Check(CurrentBoard, set.Turn))
+                                IS_CHECK = true;
+                            else
+                                IS_CHECK = false;
+
+                            if (checkmate(CurrentBoard, set.Turn))
+                                IS_CHECKMATE = true;
+                        }
+                        else
+                            warn = false;
+                    clicked = false;
+                }
+                else if (!warn)
+                    clicked = false;
+            }
+
+            if (event.mouseButton.button == Mouse::Left && !clicked)
+            {
+                int mouse_x = event.mouseButton.x;
+                int mouse_y = event.mouseButton.y;
+                if (mouse_x < set.cell_offset + 8 * set.cell_size && mouse_x > set.cell_offset && mouse_y < set.cell_offset + 8 * set.cell_size && mouse_y > set.cell_offset)
+                {
+                    r = (mouse_x - set.cell_offset) / (set.cell_size);
+                    c = 7 - (mouse_y - set.cell_offset) / (set.cell_size);
+                    if (CurrentBoard.getoccupied(r, c) and set.your_turn(r, c) and CurrentBoard.getmove(r, c).length() > 0)
+                        clicked = true;
+                    amove = CurrentBoard.getmove(r, c);
+                }
+                else
+                    clicked = false;
+            }
+
+            if (event.mouseButton.button == Mouse::Left && !clicked)
+            {
+                int mouse_x = event.mouseButton.x;
+                int mouse_y = event.mouseButton.y;
+                if (mouse_x >830  && mouse_x <990  && mouse_y > 220 && mouse_y < 370)
+                {
+                    CurrentBoard=empty;
+                    IS_CHECK=false;
+                    IS_CHECKMATE=false;
+                    restart();
+                    set.Turn=2;
+                }
+            }
 
 
         }
